a8ea1d1ed0c5302297c43f759309e04f99f0edf8
Jarkko Linnanvirta <jarkko@taitavasti.fi>
2021-03-11 18:42:08 +0200

    #4: Remove old version of PatternCompare() and install a new one from GitHub.
    The old function gave a false negative result for some test_*.cb file names.
    
    Fixes #4.

--- Install/Install-libraries.cmd
+++ Install/Install-libraries.cmd
@@ -3,6 +3,7 @@
 rem We should be able to cd back to the original directory because if the script is run from the command line, the command line would change directory to where ever we left.
 set original_directory=%cd%
 
+rem Ensure that git is available
 where git
 if %ERRORLEVEL% neq 0 (
 	echo Git is not installed. Install it from https://git-scm.com/download/win
@@ -10,12 +11,17 @@ if %ERRORLEVEL% neq 0 (
 	exit /b 1
 )
 
+rem Ensure that Libraries\ folder is available
 cd ..\Libraries
 if %ERRORLEVEL% neq 0 (
 	echo The Libraries\ folder is missing.
 	pause
 	exit /b 1
 )
+
+rem ----------------------
+rem DEFINE LIBRARIES BELOW
+rem ----------------------
 
 rem CommandLineArguments installation
 cd CommandLineArguments > nul 2> nul
@@ -28,10 +34,32 @@ if %ERRORLEVEL% neq 0 (
 )
 if %ERRORLEVEL% neq 0 (
 	echo Failed to install library CommandLineArguments!
-	pause
 ) else (
 	echo CommandLineArguments library installed/updated successfully.
-	pause
 )
 
+rem Between different libraries:
+cd %original_directory%\..\Libraries
+
+rem PatternCompare installation
+cd PatternCompare > nul 2> nul
+if %ERRORLEVEL% neq 0 (
+	rem Install it
+	git clone https://github.com/Taitava/cb-PatternCompare.git PatternCompare
+) else (
+	rem Update it
+	git pull
+)
+if %ERRORLEVEL% neq 0 (
+	echo Failed to install library PatternCompare!
+) else (
+	echo PatternCompare library installed/updated successfully.
+)
+
+rem ----------------------
+rem DEFINE LIBRARIES ABOVE
+rem ----------------------
+
+rem Finish
+pause
 cd %original_directory%
